1af81d860ec7e4eba623417469b22e96dfafd914
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-12-11 12:35:58 +0800

    每日一语

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
 const nodemailer = require('nodemailer')
 const schedule = require('node-schedule');
+const axios = require('axios').default;
 let rule = new schedule.RecurrenceRule();
-const pass = require('./config').pass;
-// rule.hour = 0;
-// rule.minute = 0;
-// rule.second = 0;
-rule.second = [0, 10, 20, 30, 40, 50]
+const config = require('./config');
+rule.hour = 10;
+rule.minute = 0;
+rule.second = 0;
+// rule.second = [0, 10, 20, 30, 40, 50]
 const transporter = nodemailer.createTransport({
     service: 'QQ',
     port: 465,
     secureConnection: true,
     auth: {
-        user: '1078156801@qq.com',
+        user: config.user,
         // 非qq密码，而是设置的smtp密码
-        pass: pass
+        pass: config.pass
     }
 });
 const mailOptions = {
-    from: '1078156801@qq.com',
-    to: '1078156801@qq.com',
-    subject: 'Hello Sir',
+    from: config.from,
+    to: config.to,
+    subject: '每日一语',
     text: 'Hello world',
-    html: '<h3>世俗的无奈</h3>'
+    html: ''
+}
+function getHoneyWords() {
+    var url = "https://chp.shadiao.app/api.php";
+    return axios.get(url);
 }
 let job = schedule.scheduleJob(rule, () => {
-    transporter.sendMail(mailOptions, function(error, info) {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: ' + JSON.stringify(info))
+    getHoneyWords().then((res) => {
+        mailOptions.html = `<h3>${res.data}</h3>`
+        transporter.sendMail(mailOptions, function(error, info) {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: ' + JSON.stringify(info))
+        })
     })
 })
